e44daf487d95ca75b3e2ebba6f4eefe2c040e31e
Edgar López <edlopmor@hotmail.es>
2021-11-28 18:53:37 +0100

    Agregar archivos de proyecto.

---
+++ StockTracking/BLL/IBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.BLL
+{
+    interface IBLL<T,K> where T:class  where K:class
+    {
+        bool Insert(T entity);
+
+        bool Update(T entity);
+
+        bool Delete(T entity);
+
+        K select();
+
+        bool GetBack(T entity);
+    }
+}
---
+++ StockTracking/DAL/DAO/IDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.DAL.DAO
+{
+    // T = tipos de datos y K = clase que utilizaremos
+    interface IDAO<T,K> where T:class where K:class
+    {
+        List<K> Select();
+        bool Insert(T entity);
+
+        bool Update(T entity);
+
+        bool Delete(T entity);
+
+        bool GetBack(int ID);
+
+    }
+}
